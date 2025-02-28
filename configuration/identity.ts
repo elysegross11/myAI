e56b701eff660cfd6e1298d08665691d8cfdc362
elysegross11 <elizabeth_gross@kenan-flagler.unc.edu>
2025-02-28 09:03:23 -0500

    Update identity.ts
    [AI TONE] changed

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -2,5 +2,5 @@ export const OWNER_NAME: string = `Elizabeth Gross`;
 export const OWNER_DESCRIPTION: string = `[OWNER DESCRIPTION]`;
 
 export const AI_NAME: string = `AI Brand Builder`;
-export const AI_TONE: string = `[AI TONE]`;
+export const AI_TONE: string = `Business Professional`;
 export const AI_ROLE: string = `[AI ROLE]`;
