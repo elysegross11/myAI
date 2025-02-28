4752a9628f74d62352b818d591c2c6f1d6a3d0d7
elysegross11 <elizabeth_gross@kenan-flagler.unc.edu>
2025-02-28 08:52:06 -0500

    Update ui.ts
    changed page title from [PAGE TITLE]

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -4,7 +4,7 @@ export const CHAT_HEADER: string = `AI SBM`;
 export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
 export const FOOTER_MESSAGE: string = `Consumer is King!`;
 export const CLEAR_BUTTON_TEXT: string = `Clear Text`;
-export const PAGE_TITLE: string = `[PAGE TITLE]`;
+export const PAGE_TITLE: string = `A Brand Builders Best Friend`;
 export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
