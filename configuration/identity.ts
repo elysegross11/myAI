b14d4ff0b9dec48de59c72cb405e97784272a0ee
elysegross11 <elizabeth_gross@kenan-flagler.unc.edu>
2025-02-28 09:12:00 -0500

    Update identity.ts
    changed from [OWNER DESCRIPTION]

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,5 +1,5 @@
 export const OWNER_NAME: string = `Elizabeth Gross`;
-export const OWNER_DESCRIPTION: string = `[OWNER DESCRIPTION]`;
+export const OWNER_DESCRIPTION: string = `Senior Brand Manager for Vicks`;
 
 export const AI_NAME: string = `AI Brand Builder`;
 export const AI_TONE: string = `Business Professional`;
